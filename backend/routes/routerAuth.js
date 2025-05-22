a41e9574bca1c16ab20864d490c53b25cc324363
ShinHarry <quanty66@gmail.com>
2025-05-22 22:58:11 +0700

    update

--- backend/routes/routerAuth.js
+++ backend/routes/routerAuth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User"); // Đường dẫn model User
 require("dotenv").config();
 
