109d8a550bdfaf6acb52ef2ac3436c50f7f9f098
ShinHarry <quanty66@gmail.com>
2025-05-24 17:04:37 +0700

    fix

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,5 +1,4 @@
 const cloudinary = require("cloudinary").v2;
-const fs = require("fs");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,15 +6,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadToCloudinary = async (file, folder, namePrefix) => {
-  const result = await cloudinary.uploader.upload(file.path, {
-    folder: folder,
-    public_id: `${namePrefix}-${Date.now()}`,
-  });
-  fs.unlinkSync(file.path); // Xoá ảnh tạm local
-  return result.secure_url; // Link ảnh Cloudinary
-};
-
-module.exports = {
-  uploadToCloudinary,
-};
+module.exports = cloudinary;
